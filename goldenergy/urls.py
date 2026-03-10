64533730c5e34556c6067a497a98281b16c87714
JoelCaquene <joelcaquenejr@gmail.com>
2026-03-10 17:38:27 +0100

    Fix: permitindo que imagens de mídia sejam servidas em produção

--- goldenergy/urls.py
+++ goldenergy/urls.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
-# CORREÇÃO: Adiciona a rota de mídia (comprovativos) para ser executada 
-# mesmo quando DEBUG=False.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Configuração para servir arquivos de Mídia (Comprovativos)
+# Isso permite que o link /media/... funcione mesmo no Render (DEBUG=False)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+else:
+    # Em desenvolvimento local (DEBUG=True)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
